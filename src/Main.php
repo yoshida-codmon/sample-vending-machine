5dd8b92a8f5826742f24348e68d911ce9a755397
Yoshiaki Yoshida <y_yoshida@codmon.co.jp>
2023-03-11 22:40:51 +0900

    Mainに処理をべた書きで仮実装

--- src/Main.php
+++ src/Main.php
@@ -19,7 +19,15 @@ class Main
      */
     public static function runSimply(array $coins, string $menu): string
     {
-        return "do implementation";
+        // - お釣り用のお金
+        $moneyInventory = [
+            500 => 999,
+            100 => 999,
+            50 => 999,
+            10 => 999,
+        ];
+
+        return self::run($moneyInventory, ['coins' => $coins, 'menu' => $menu]);
     }
 
     /**
@@ -33,6 +41,66 @@ class Main
      */
     public static function run(array $vendingMachineCoins, array $userInput): string
     {
-        return "do implementation";
+        // - お釣り用のお金
+        $moneyInventory = [];
+        foreach ($vendingMachineCoins as $key => $value) {
+            $moneyInventory[(int)$key] = $value;
+        }
+        $coins = $userInput['coins'];
+        $menu = $userInput['menu'];
+
+        // 料金表から商品の料金を検索する
+        // - 料金表
+        $menuPriceMap = [
+            'cola' => 120,
+            'coffee' => 150,
+            'energy_drink' => 210
+        ];
+
+        if (!array_key_exists($menu, $menuPriceMap)) {
+            return "error: Not found in the menu list. [$menu]";
+        }
+        $price = $menuPriceMap[$menu];
+
+        // お釣りを計算する
+        // - 投入金額の合計額を計算
+        $paid = array_reduce(array_keys($coins), function ($paid, $coin) use($coins) {
+            return $paid + (int) $coin * $coins[$coin];
+        }, 0);
+
+        // - 金額不足のエラーチェック
+        if ($paid < $price) {
+            return "error: Insufficient paid amount";
+        }
+
+        $coinTypes = array_keys($moneyInventory);
+        arsort($coinTypes);
+
+        $change = $paid - $price;
+        if ($change === 0) {
+            return 'nochange';
+        }
+
+        $changeCoins = [];
+        foreach ($coinTypes as $coinType) {
+            $count = (int) floor($change / $coinType);
+            $count = min($moneyInventory[$coinType], $count);
+            if ($count > 0) {
+                $changeCoins[$coinType] = $count;
+                $change -= $coinType * $count;
+            }
+        }
+
+        if ($change > 0) {
+            return 'error: Sorry,we dont have any change';
+        }
+
+        // 出力の書式をフォーマットする
+        $results = [];
+        foreach ($changeCoins as $coin => $count) {
+            $results[] = "$coin $count";
+        }
+
+        return implode(' ', $results);
     }
 }
